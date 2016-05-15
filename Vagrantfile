2d5cbecec852f2785cb3d43cebeb89c2e6e6fb5e
Divya van Mahajan <divyavanmahajan@users.noreply.github.com>
2016-05-15 11:36:23 -0700

    Amazon VPC support added. Switch to Ubuntu 16.04 Xenial. Switch to Jetty8, JDK8, Postgres 9.5, CKAN 2.5.2

--- Vagrantfile
+++ Vagrantfile
@@ -6,24 +6,39 @@ VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
 
-  config.vm.box = "chef/ubuntu-13.10"
+  config.vm.box = "chef/ubuntu-16.04"
   config.vm.network :private_network, ip: "192.168.33.10"
 
   config.vm.provider :aws do |aws, override|
     override.vm.box = "dummy"
     override.vm.box_url = "https://github.com/mitchellh/vagrant-aws/raw/master/dummy.box"
 
+    override.ssh.private_key_path = ENV['AWS_EC2_KEYPAIR_PATH']
     aws.access_key_id     = ENV['AWS_ACCESS_KEY']
     aws.secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
     aws.keypair_name      = ENV['AWS_EC2_KEYPAIR']
-
+    aws.subnet_id = ENV['AWS_SUBNET']
     aws.region = "us-east-1"
-    aws.ami = "ami-35dbde5c" # Ubuntu13.10 64bit
+    aws.ami = "ami-840910ee" # Ubuntu 16.04 hvm:ebs-ssd
 
-    aws.instance_type = "t1.micro"
+# http://cloud-images.ubuntu.com/locator/ec2/
+    # The following need the 0.8.0 vagrant-aws plugin which is not released.
+    #aws.aws_profile = ENV['AWS_DEFAULT_PROFILE']
+    #aws.region_config "us-east-1", :ami => "ami-12345678"
+    #aws.region_config "us-west-2", :ami => "ami-fa82739a"
+    #aws.region_config "us-west-1", :ami => "ami-d8e996b8"
+    #aws.region_config "eu-west-1", :ami => "ami-3079f543"
+    #aws.region_config "eu-central-1", :ami => "ami-f0eb089f"
 
+    aws.tags = { 
+	'Name' => 'CKAN-xlarge'
+    }
+
+    aws.instance_type = "m4.xlarge"
+
+    #aws.block_device_mapping = [{ 'DeviceName' => '/dev/sda1', 'Ebs.VolumeSize' => 50 }]
+    #aws.user_data = File.read("user_data.txt")
     override.ssh.username = "ubuntu"
-    override.ssh.private_key_path = ENV['AWS_EC2_KEYPAIR_PATH']
   end
 
   config.ssh.pty = true
--- bootstrap.sh
+++ bootstrap.sh
@@ -16,15 +16,16 @@ apt-get -y upgrade
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 locale-gen en_US.UTF-8
-dpkg-reconfigure locales
+dpkg-reconfigure -f noninteractive locales
 
 
 # Installing CKAN from Source
 # http://docs.ckan.org/en/ckan-2.2/install-from-source.html
 
+# Turn off Apache
+(systemctl kill apache2.service || exit 0;)
 # Install the required packages
-
-apt-get install -y nginx apache2 libapache2-mod-wsgi python-dev postgresql libpq-dev python-pip python-virtualenv git-core solr-jetty openjdk-6-jdk python-pastescript pwgen
+apt-get install -y nginx apache2 libapache2-mod-wsgi python-dev postgresql libpq-dev python-pip python-virtualenv git-core solr-jetty openjdk-8-jdk python-pastescript pwgen
 
 PASSWD=`pwgen 12 1`
 
@@ -38,7 +39,8 @@ chown `whoami` /usr/lib/ckan/default
 virtualenv --no-site-packages /usr/lib/ckan/default
 . /usr/lib/ckan/default/bin/activate
 
-pip install -e 'git+https://github.com/okfn/ckan.git@ckan-2.2#egg=ckan'
+pip install -e 'git+https://github.com/ckan/ckan.git@ckan-2.5.2#egg=ckan'
+pip install testrepository
 pip install -r /usr/lib/ckan/default/src/ckan/requirements.txt
 deactivate
 . /usr/lib/ckan/default/bin/activate
@@ -48,7 +50,7 @@ deactivate
 sudo -u postgres createuser -S -D -R ckan_default
 sudo -u postgres createdb -O ckan_default ckan_default -E utf-8
 echo "ALTER USER ckan_default WITH PASSWORD '$PASSWD';" | sudo -u postgres psql
-install -o postgres -g postgres -m 0644 /vagrant/files/pg_hba.conf /etc/postgresql/9.1/main/
+install -o postgres -g postgres -m 0644 /vagrant/files/pg_hba.conf /etc/postgresql/9.5/main/
 service postgresql restart
 
 # Create a CKAN config file
@@ -58,21 +60,21 @@ chown -R www-data /etc/ckan/
 
 cd /usr/lib/ckan/default/src/ckan
 paster make-config ckan /etc/ckan/default/development.ini
-sed -e "s/ckan_default:pass/ckan_default:$PASSWD/" /etc/ckan/default/development.ini > /etc/ckan/default/production.ini
+sed -e "s/ckan_default:pass/ckan_default:$PASSWD/" -e 's/ckan.site_url =/ckan.site_url=http:\/\/ec2-52-91-122-145.compute-1.amazonaws.com/' /etc/ckan/default/development.ini > /etc/ckan/default/production.ini
 
 # Setup Solr (Single Solr instance)
 
-install -o root -g root -m 0644 /vagrant/files/jetty /etc/default/
+install -o root -g root -m 0644 /vagrant/files/jetty /etc/default/jetty8
 
 if [ -f /etc/solr/conf/schema.xml ]; then
     mv /etc/solr/conf/schema.xml /etc/solr/conf/schema.xml.bak
 fi
 
 if [ ! -L /etc/solr/conf/schema.xml ]; then
-    ln -s /usr/lib/ckan/default/src/ckan/ckan/config/solr/schema-2.0.xml /etc/solr/conf/schema.xml
+    ln -s /usr/lib/ckan/default/src/ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
 fi
 
-sudo service jetty restart
+sudo service jetty8 restart
 
 # Create database tables
 
@@ -80,6 +82,10 @@ cd /usr/lib/ckan/default/src/ckan
 paster db init -c /etc/ckan/default/production.ini
 
 # Link to who.ini
+
+if [ -f /etc/ckan/default/who.ini ]; then
+    mv /etc/ckan/default/who.ini /etc/ckan/default/who.ini.bak
+fi
 
 ln -s /usr/lib/ckan/default/src/ckan/who.ini /etc/ckan/default/who.ini
 
