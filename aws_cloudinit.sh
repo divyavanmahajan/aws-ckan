96820f03c27123d0a571042ee3b5d8694df9455d
Divya van Mahajan <divyavanmahajan@users.noreply.github.com>
2016-05-15 21:38:31 -0700

    Removing bootstrap from cloudinit since Postgres is not coming up

--- aws_cloudinit.sh
+++ aws_cloudinit.sh
@@ -2,6 +2,9 @@
 echo 'got user data' > /tmp/user_data.log
 git clone https://github.com/divyavanmahajan/aws-ckan.git /vagrant
 cd /vagrant
-bash bootstrap.sh
-# The bootstrap must be done by logging into the system.
+# bash bootstrap.sh
+# During CloudInit, the postgres database does not come up. So it seems that the bootstrap
+# must be done interactively?
+# The bootstrap is run logging into the system.
+# cd /vagrant
 # sudo bash bootstrap.sh
